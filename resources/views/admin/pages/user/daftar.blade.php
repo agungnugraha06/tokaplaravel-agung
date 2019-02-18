ddb4c065f9822646adb61a60aad69a9eeb5d6189
agungnugrah06 <nugrahaace122@gmail.com>
2019-02-18 09:43:22 +0700

    penambahan perintah if session untuk delete&failde

--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -22,6 +22,24 @@
 </div>
 @endif
 
+@if(session('result') == 'delete')
+<div class="alert alert-success alert-dismissible fade show">
+     <strong>Deleted!</strong> Berhasil dihapus.
+     <button type="button" class="close" data-dismiss="alert">
+          &times;
+     </button>
+</div>
+@endif
+
+@if(session('result') == 'fail-delete')
+<div class="alert alert-danger alert-dismissible fade show">
+     <strong>Failde!</strong> Gagal Dihapus.
+     <button type="button" class="close" data-dismiss="alert">
+          &times;
+     </button>
+</div>
+@endif
+
 <div class="row">
 	<div class="col-md-6 mb-3">
 		<a href="{{ route('admin.user.add') }}" class="btn btn-primary">[+] Tambah</a>
@@ -95,7 +113,7 @@
 
               <div class="modal-body">
                  Apakah anda yakin ingin menghapusnya?
-                  <form id="form-delete" method="post" action="#">
+                  <form id="form-delete" method="post" action="{{ route('admin.user') }}">
                       {{ csrf_field() }}
                       {{ method_field('delete') }}
                       <input type="hidden" name="id" id="input-id">
@@ -123,7 +141,7 @@ $(function(){
     });
 
     $('.btn-delete').click(function(){
-        alert( $('#input-id').val() );
+        $('#form-delete').submit();
     });
 
 })
